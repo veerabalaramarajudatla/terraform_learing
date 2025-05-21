d06fe4dc709eb686c60a8ef0663fe25c9af4dbed
Veera Balarama Raju Datla <datla@achala.local>
2025-05-21 14:28:22 +0530

    s3 resource added

--- main.tf
+++ main.tf
@@ -9,5 +9,12 @@ terraform {
 }
 
 provider "aws" {
-    region = var.region
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "bucket_details" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
 variable "region"{
     type = string
 }
+
+variable "access_key"{
+    type = string
+}
+
+variable "secret_key"{
+    type = string
+}
+
+variable "bucket_name"{
+    type = string
+}
